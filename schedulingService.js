31e0567f4c7bf8300997f38712fae8278c3f0da4
alperengokbak <swalperen3008@gmail.com>
2024-01-13 18:38:34 +0300

    Changed rabbitmqhost

--- schedulingService.js
+++ schedulingService.js
@@ -7,7 +7,7 @@ dotenv.config();
 export default async function consumeFromRabbitMQ() {
   try {
     const connection = await amqp.connect(
-      `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}`
+      `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`
     );
     console.log(connection);
     console.log("Connected to RabbitMQ");
