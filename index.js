7d66680c8a7192410d08874193692ffd55a36ee9
alperengokbak <swalperen3008@gmail.com>
2024-01-13 04:01:31 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import consumeFromRabbitMQ from "./schedulingService.js";
+import cron from "node-cron";
+
+const app = express();
+const PORT = 3001;
+
+var corsOptions = {
+  origin: ["http://localhost:3002", "http://localhost:3000"],
+  methods: "GET",
+};
+
+app.use(cors(corsOptions));
+
+app.get("/trigger-task", async (req, res) => {
+  try {
+    await consumeFromRabbitMQ();
+    res.send("Task triggered successfully!");
+  } catch (error) {
+    console.error("Error triggering task:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+cron.schedule(
+  "0 1 * * *",
+  async () => {
+    try {
+      await consumeFromRabbitMQ();
+    } catch (error) {
+      console.error("Error in scheduled task:", error);
+    }
+  },
+  {
+    timezone: "Europe/Istanbul",
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Payment Service running on port ${PORT}`);
+});
---
+++ schedulingService.js
@@ -0,0 +1,66 @@
+import amqp from "amqplib";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default async function consumeFromRabbitMQ() {
+  try {
+    const connection = await amqp.connect("amqp://localhost");
+    console.log("Connected to RabbitMQ");
+
+    const channel = await connection.createChannel();
+    console.log("Channel created");
+
+    const queue = "prescription_queue";
+    await channel.assertQueue(queue, { durable: true });
+    console.log(`Queue '${queue}' asserted`);
+
+    channel.consume(
+      queue,
+      async (message) => {
+        const prescriptionData = JSON.parse(message.content.toString());
+        console.log(`Received prescription message: ${JSON.stringify(prescriptionData)}`);
+
+        await sendEmailReport(
+          prescriptionData.pharmacyName,
+          prescriptionData.totalAmount,
+          prescriptionData.numberOfPrescriptions
+        );
+
+        channel.ack(message);
+      },
+      { noAck: false }
+    );
+  } catch (error) {
+    console.error("Error consuming messages from RabbitMQ:", error);
+  }
+}
+
+const sendEmailReport = async (pharmacyName, totalAmount, numberOfPrescriptions) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.GMAIL,
+        pass: process.env.APP_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.GMAIL,
+      to: process.env.RECEIVER_GMAIL,
+      subject: "Daily Prescription Report",
+      text: `To: ${pharmacyName}\nYou have submitted ${numberOfPrescriptions} prescriptions today\nTotal amount is ${totalAmount} TL`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    console.log(`Email sent to ${pharmacyName}`);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
